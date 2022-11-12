9c005e92841f9145994e0ec0cbd5e930f5971e2b
pullpanther <softechstar.wlc@outlook.com>
2022-11-12 22:06:56 +0500

    Customised the home page and logout function.

--- market/__init__.py
+++ market/__init__.py
@@ -9,6 +9,9 @@ app.config['SECRET_KEY'] = 'b12a02fe46eec323ee404744'
 
 db = SQLAlchemy(app)
 bcrypt = Bcrypt(app)
+
 login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = 'info'
 
 from market import routes
