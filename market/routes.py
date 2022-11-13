3d488035d419f3664f9252b0f3c6f2ea0b9fa327
pullpanther <softechstar.wlc@outlook.com>
2022-11-13 21:02:57 +0500

    Updated the purchasing button and customized the purchase item feature

--- market/routes.py
+++ market/routes.py
@@ -1,19 +1,33 @@
 from market import app, db
-from flask import render_template, redirect, url_for, flash
+from flask import render_template, redirect, url_for, flash, request
 from market.models import Item, User
-from market.forms import RegisterForm, LoginForm
-from flask_login import login_user, logout_user, login_required
+from market.forms import RegisterForm, LoginForm, PurchaseItemForm
+from flask_login import login_user, logout_user, login_required, current_user
 
 @app.route('/')
 @app.route('/home')
 def home_page():
     return render_template('home.html')
 
-@app.route('/market')
+@app.route('/market', methods=['GET', 'POST'])
 @login_required
 def market_page():
-    items = Item.query.all()
-    return render_template('market.html', items=items)
+    purchase_form = PurchaseItemForm()
+    if request.method == "POST":
+        purchased_item = request.form.get('purchased_item')
+        p_item_object = Item.query.filter_by(name=purchased_item).first()
+        if p_item_object:
+            if current_user.can_purchase(p_item_object):
+                p_item_object.buy(current_user)
+                flash(f"You purchased {p_item_object.name} for ₹ {p_item_object.price}.",category='success')
+            else:
+                flash(f"You don't have enough money to purchase {p_item_object.name}", category='danger')
+
+        return redirect(url_for('market_page'))
+    
+    if request.method == "GET":  
+        items = Item.query.filter_by(owner=None)
+        return render_template('market.html', items=items, purchase_form=purchase_form)
 
 @app.route('/register', methods=['GET', 'POST'])
 def register_page():
